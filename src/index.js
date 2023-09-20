07bfa8b1aa2e92047309054619b20edc13e7f0c0
AlexONeillTI <alex.oneill@thoughtindustries.com>
2023-09-20 10:25:20 +0000

    process port

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors'); // Import the cors middleware
 
 const app = express();
-const PORT = 8001;
+const PORT = process.env.PORT || 8081;
 
 app.use(express.json());
 app.use(cors());
